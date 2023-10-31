73b23b90510dada3222fc042dcdf186776158ccf
MKAbdElrahman <mkcodedev@gmail.com>
2023-10-31 03:54:53 +0200

    drafted the Node  skeleton

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.21.1
 
 require github.com/google/uuid v1.4.0
 
-require github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
+require (
+	github.com/gofrs/uuid v4.4.0+incompatible
+	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
+)
 
 require (
 	github.com/docker/go-connections v0.4.0
--- manager/manager.go
+++ manager/manager.go
@@ -1,1 +1,35 @@
 package manager
+
+import (
+	"fmt"
+	"monazzim/task"
+
+	"github.com/gofrs/uuid"
+	"github.com/golang-collections/collections/queue"
+)
+
+type Manager struct {
+	Pending       queue.Queue
+	TaskDb        map[string]task.Task
+	EventDb       map[string]task.TaskEvent
+	Workers       []string
+	WorkerTaskMap map[string][]uuid.UUID
+	TaskWorkerMap map[uuid.UUID]string
+}
+
+// responsible for looking at the requirements specified in a Task
+// and evaluating the resources available in the
+// pool of workers to see which worker is best suited to run the task.
+func (m *Manager) SelectWorker() {
+	fmt.Println("I will select an appropriate worker")
+}
+
+// Keep track of tasks, their states, and the machine on which they run.
+func (m *Manager) UpdateTasks() {
+	fmt.Println("I will update tasks")
+}
+
+// send tasks to workers
+func (m *Manager) SendWork() {
+	fmt.Println("I will send work to workers")
+}
