ca122ad1bf84744ac842cd70b110425fa7b7fd95
MKAbdElrahman <mkcodedev@gmail.com>
2023-10-31 03:07:52 +0200

    drafted the worker skeleton

--- go.mod
+++ go.mod
@@ -4,4 +4,9 @@ go 1.21.1
 
 require github.com/google/uuid v1.4.0
 
-require github.com/docker/go-connections v0.4.0 // indirect
+require github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
+
+require (
+	github.com/docker/go-connections v0.4.0
+	github.com/stretchr/testify v1.8.4 // indirect
+)
--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,35 @@
 package worker
+
+import (
+	"fmt"
+	"monazzim/task"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+// Worker struct represents a system component responsible for running tasks as Docker containers,
+// receiving task assignments from a manager, providing statistics for scheduling purposes,
+// and maintaining task state information.
+type Worker struct {
+	Name      string
+	Queue     queue.Queue
+	Db        map[uuid.UUID]task.Task
+	TaskCount int
+}
+
+func (w *Worker) RunTask() {
+	fmt.Println("I will start or stop a task")
+}
+
+func (w *Worker) StartTask() {
+	fmt.Println("I will start a task")
+}
+
+func (w *Worker) StopTask() {
+	fmt.Println("I will stop a task")
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("I will collect stats")
+}
